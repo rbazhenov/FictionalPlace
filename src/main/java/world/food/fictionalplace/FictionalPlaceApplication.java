07e02d4fb8365d0226ee4e0ff8445a43d90b4714
RBazhenov <rbazhenov@croc.ru>
2022-08-17 23:31:53 +0500

    Initial commit

---
+++ src/main/java/world/food/fictionalplace/FictionalPlaceApplication.java
@@ -0,0 +1,13 @@
+package world.food.fictionalplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FictionalPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FictionalPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/world/food/fictionalplace/FictionalPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package world.food.fictionalplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FictionalPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
